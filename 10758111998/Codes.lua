8a63f4cfb55c2260c17837baf280eed8d70f1f45
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-11-21 13:29:06 -0500

    Update `11/21/2022`

---
+++ 10758111998/Codes.lua
@@ -0,0 +1,14 @@
+--[[
+    Codes for 2019 Booga Booga Reborn
+
+    Updated: 11/21/2022
+]]
+
+local LastUpdated = "11/21/2022"
+local Codes = {
+    "25KLikes",
+    "15MVISITS",
+    "69KFavorites"
+}
+
+return Codes, LastUpdated
